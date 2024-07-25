1d938a1fdf51700937bcab2bb9333d6710eb52c6
kseen <korenevdn@gmail.com>
2024-07-25 20:17:43 +0300

    chore: Refactor Ansible installation script
    The Ansible installation script has been refactored to separate the installation and playbook execution steps. This allows for better modularity and reusability.

---
+++ ansible-install-deb.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Description: Install Ansible and run the playbook
+apt update && apt upgrade -y && apt install ansible -y
--- ansible-install-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Description: Install Ansible and run the playbook
-apt update && apt upgrade -y && apt install ansible -y && ansible-playbook install-vpn-assembly-debian.yml
